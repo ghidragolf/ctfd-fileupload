4639ecaafa107c91d5947207f8c9adec0c2bdd77
ghidragolf <ghidragolf>
2023-02-08 21:11:16 -0500

    try/except for service not starting right away

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -10,12 +10,16 @@ def main():
     # Use the same AMQP as the CTFd docker image
     rabbit_url = os.environ.get("RABBITMQ_URL", "amqp://user:pass@rabbit:5672")
     q = os.environ.get("RABBITMQ_QUEUE", "ctfd")
-    connection = pika.BlockingConnection(pika.URLParameters(rabbit_url))
-    channel = connection.channel()
-    channel.queue_declare(queue=q)
-    channel.basic_consume(queue=q, auto_ack=True, on_message_callback=callback)
-    print("[*] Waiting for messages")
-    channel.start_consuming()
+    try:
+        connection = pika.BlockingConnection(pika.URLParameters(rabbit_url))
+        channel = connection.channel()
+        channel.queue_declare(queue=q)
+        channel.basic_consume(queue=q, auto_ack=True, on_message_callback=callback)
+        print("[*] Waiting for messages")
+        channel.start_consuming()
+    except:
+        time.sleep(10)
+        main()
 
 def callback(ch, method, properties, body):
     data = json.loads(body.decode("utf-8"))
